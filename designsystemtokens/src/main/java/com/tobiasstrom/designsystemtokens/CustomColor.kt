03265627aa2fe1a78198619ec4fd062c2e62c8c3
Tobias Strøm <tobias2409@gmail.com>
2022-05-31 16:38:50 +0200

    Initial commit

---
+++ designsystemtokens/src/main/java/com/tobiasstrom/designsystemtokens/CustomColor.kt
@@ -0,0 +1,15 @@
+package com.tobiasstrom.designsystemtokens
+
+import androidx.compose.ui.graphics.Color
+
+object CustomColor {
+    val primaryDeepBlue900 = Color(0xFF10132F)
+    val primaryDeepBlue800 = Color(0xFF1B1F4A)
+    val primaryDeepBlue700 = Color(0xFF313663)
+    val primaryDeepBlue600 = Color(0xFF6D7196)
+    val primaryDeepBlue500 = Color(0xFF8D91AF)
+    val primaryDeepBlue400 = Color(0xFFABAEC5)
+    val primaryDeepBlue300 = Color(0xFFD8DAE8)
+    val primaryDeepBlue200 = Color(0xFFE7E8F3)
+    val primaryDeepBlue100 = Color(0xFFF7F7FD)
+}
